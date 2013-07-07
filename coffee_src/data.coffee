72e368fe564377086fe8fa70d2d861ee4012158e
Luis Crespo <lcrespo@es.ibm.com>
2013-07-07 09:19:11 +0200

    Basic GET queries working

--- coffee_src/data.coffee
+++ coffee_src/data.coffee
@@ -1,13 +1,15 @@
 # Requires
-mongo = require('mongodb').MongoClient
+MongoClient = require('mongodb').MongoClient
+ObjectID = require('mongodb').ObjectID
 url = require('url')
 utils = require('./utils')
+_ = require('underscore')
 
 db = null
 
 # Creates a connection with the mongo server
 connectDB = (callback) ->
-	mongo.connect(exports.dbConfig.url, (err, dbConnection) ->
+	MongoClient.connect(exports.dbConfig.url, (err, dbConnection) ->
 		db = dbConnection
 		callback(err)
 	)
@@ -15,20 +17,49 @@ connectDB = (callback) ->
 # Database connect middleware function
 dataHandler = (req, res, next) ->
 	switch req.method
-		when 'GET' then doGet(req, res)
-		when 'POST' then doPost(req, res)
-		else next('Method not supported')
+		when 'GET' then doGet(req, res, next)
+		when 'POST' then doPost(req, res, next)
+		else next("Method '#{req.method}' not supported")
 
-doGet = (req, res) ->
+doGet = (req, res, next) ->
 	parsedUrl = url.parse(req.url, true)
-	console.log(parsedUrl)
-	#TODO inspect URL and make db query accordingly
-	res.writeHead(200, 'Content-Type': 'application/json')
-	res.end(utils.jsonStringifyNoCircular(req))
+	console.log('\n--- get ---')
+	console.log(parsedUrl.pathname, parsedUrl.query)
+	pathParts = parsedUrl.pathname.split('/')
+	collName = pathParts[1]
+	if not collName
+		next("Invalid request path: #{parsedUrl.pathname} - missing collection name")
+		return
+	collection = db.collection(collName)
+	console.log("Collection: #{collection.collectionName}")
+	if _.isEmpty(parsedUrl.query)
+		oid = pathParts[2]
+		if oid
+			# Direct fetch of oid collection
+			collection.findOne(_id: ObjectID(oid), (err, item) ->
+				respondJson(res, { err: err, item: item }))
+		else
+			# List all elements in collection
+			collection.find().toArray((err, items) ->
+				respondJson(res, { err: err, items: items }))
+	else
+		# Find all items in collection that match a query
+		collection.find(parsedUrl.query).toArray((err, items) ->
+				respondJson(res, { err: err, items: items }))
 
 doPost = (req, res) ->
+	#TODO support create, delete and update
 	throw 'Not supported'
 
+respondJson = (res, obj) ->
+	res.writeHead(200, 'Content-Type': 'application/json')
+	if not obj
+		res.end('<null>')
+	else if obj.constructor == String
+		#TODO remove later
+		res.end(obj)	
+	else
+		res.end(utils.jsonStringifyNoCircular(obj))	
 
 exports.openDbConnection = connectDB
 exports.connectHandler = dataHandler
--- data.js
+++ data.js
@@ -1,16 +1,20 @@
 (function() {
-  var connectDB, dataHandler, db, doGet, doPost, mongo, url, utils;
+  var MongoClient, ObjectID, connectDB, dataHandler, db, doGet, doPost, respondJson, url, utils, _;
 
-  mongo = require('mongodb').MongoClient;
+  MongoClient = require('mongodb').MongoClient;
+
+  ObjectID = require('mongodb').ObjectID;
 
   url = require('url');
 
   utils = require('./utils');
 
+  _ = require('underscore');
+
   db = null;
 
   connectDB = function(callback) {
-    return mongo.connect(exports.dbConfig.url, function(err, dbConnection) {
+    return MongoClient.connect(exports.dbConfig.url, function(err, dbConnection) {
       db = dbConnection;
       return callback(err);
     });
@@ -19,26 +23,71 @@
   dataHandler = function(req, res, next) {
     switch (req.method) {
       case 'GET':
-        return doGet(req, res);
+        return doGet(req, res, next);
       case 'POST':
-        return doPost(req, res);
+        return doPost(req, res, next);
       default:
-        return next('Method not supported');
+        return next("Method '" + req.method + "' not supported");
     }
   };
 
-  doGet = function(req, res) {
-    var parsedUrl;
+  doGet = function(req, res, next) {
+    var collName, collection, oid, parsedUrl, pathParts;
     parsedUrl = url.parse(req.url, true);
-    console.log(parsedUrl);
-    res.writeHead(200, {
-      'Content-Type': 'application/json'
-    });
-    return res.end(utils.jsonStringifyNoCircular(req));
+    console.log('\n--- get ---');
+    console.log(parsedUrl.pathname, parsedUrl.query);
+    pathParts = parsedUrl.pathname.split('/');
+    collName = pathParts[1];
+    if (!collName) {
+      next("Invalid request path: " + parsedUrl.pathname + " - missing collection name");
+      return;
+    }
+    collection = db.collection(collName);
+    console.log("Collection: " + collection.collectionName);
+    if (_.isEmpty(parsedUrl.query)) {
+      oid = pathParts[2];
+      if (oid) {
+        return collection.findOne({
+          _id: ObjectID(oid)
+        }, function(err, item) {
+          return respondJson(res, {
+            err: err,
+            item: item
+          });
+        });
+      } else {
+        return collection.find().toArray(function(err, items) {
+          return respondJson(res, {
+            err: err,
+            items: items
+          });
+        });
+      }
+    } else {
+      return collection.find(parsedUrl.query).toArray(function(err, items) {
+        return respondJson(res, {
+          err: err,
+          items: items
+        });
+      });
+    }
   };
 
   doPost = function(req, res) {
     throw 'Not supported';
+  };
+
+  respondJson = function(res, obj) {
+    res.writeHead(200, {
+      'Content-Type': 'application/json'
+    });
+    if (!obj) {
+      return res.end('<null>');
+    } else if (obj.constructor === String) {
+      return res.end(obj);
+    } else {
+      return res.end(utils.jsonStringifyNoCircular(obj));
+    }
   };
 
   exports.openDbConnection = connectDB;
